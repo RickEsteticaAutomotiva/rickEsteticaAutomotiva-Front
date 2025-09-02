a2edda8f0cf22dc8f4b9ba0e2396f4dd303f5738
Moises Henry <moises@Moisess-MacBook-Pro.local>
2025-09-02 19:50:04 -0300

    Adiciona css para telas nao mobile

--- public/js/services/authService.js
+++ public/js/services/authService.js
@@ -2,15 +2,14 @@ import { config } from "../config.js";
 
 export async function login(email, senha) {
     try {
-        const response = await fetch(`${config.api_url}/pessoa`, {
+        const response = await fetch(`${config.api_url}/pessoa/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({ email, senha })
         });
-
-        if (response.headers.get("content-length") === "0" || response.status === 204) {
+        if (response.headers.get("content-length") === "0" || response.status === 404) {
             return undefined;
         }
 
@@ -21,8 +20,6 @@ export async function login(email, senha) {
 }
 
 export async function cadastrar(nome, cpf, email, telefone, senha) {
-    console.log(JSON.stringify({ nome, cpf, email, telefone, senha }));
-
     try {
         const response = await fetch(`${config.api_url}/pessoa`, {
             method: 'POST',
@@ -32,11 +29,11 @@ export async function cadastrar(nome, cpf, email, telefone, senha) {
             body: JSON.stringify({ nome, cpf, email, telefone, senha })
         });
 
-        if (response.headers.get("content-length") === "0" || response.status === 204) {
+        if (response.status > 400) {
             return undefined;
         }
 
-        return await response.ok();
+        return response.ok;
     } catch (error) {
         console.error('Erro ao cadastrar:', error);
     }
